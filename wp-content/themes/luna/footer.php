68c20c36c466324976230216e64afcd4b741371b
Dong <dong@applyboard.com>
2017-09-21 11:25:06 -0400

    import bootstrap css

--- wp-content/themes/luna/footer.php
+++ wp-content/themes/luna/footer.php
@@ -13,174 +13,128 @@
 
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-    	<div class="grid grid-pad">
-        	<?php if ( get_theme_mod( 'luna_footer_title' ) ) : ?>
 
-				<h3 class="footer-contact-title"><?php echo wp_kses_post( get_theme_mod( 'luna_footer_title' )); ?></h3>
-
-			<?php endif; ?>
-
-            <?php if ( get_theme_mod( 'luna_footer_text' ) ) : ?>
-
-				<div class="footer-contact">
-					<?php echo wp_kses_post( get_theme_mod( 'luna_footer_text' )); ?>
-                </div>
-
-			<?php endif; ?>
-        </div>
 		<div class="site-info">
-            <div class="grid grid-pad">
-                <div class="col-1-1">
 
-                    	<div class="col-1-2">
+							<div class="row text-center-sm" style="margin-top:20px; margin-bottom:20px;">
+							  <div class="col-md-6">
+							    <div class="row">
+							      <div class="col-sm-4">
+							        <span class="footer-title">Welcome</span>
+							        <br>
+											<a href="https://www.applyboard.com/quick_search">Find Programs</a>
+							          <br>
+												<a href="https://www.applyboard.com/login">Login</a>
+							          <br>
+												<a href="https://www.applyboard.com/register">Register</a>
+							        	<br>
+												<a href="https://www.applyboard.com/forgot_password">Reset Password</a>
+								        <br/>
+							        <br/>
+							      </div>
 
-                        	<?php if( get_theme_mod( 'active_byline' ) == '') : ?>
+							      <div class="col-sm-4">
+							        <span class="footer-title">About</span>
+							        <br>
+											<a href="https://www.applyboard.com/about_us">Company</a>
+							        <br/>
+							        <a href="https://www.applyboard.com/features">How it works</a>
+							        <br/>
+							      </div>
 
-							<?php if ( get_theme_mod( 'luna_footerid' ) ) : ?>
+							      <div class="col-sm-4">
+							        <span class="footer-title">Partners</span>
+							        <br>
+											<a href="https://www.applyboard.com/schools">Schools</a>
+							        <br>
+											<a href="https://www.applyboard.com/partner_with_us">Partner with Us</a>
+							        <br>
+											<a href="https://www.applyboard.com/certificates">Certificates</a>
+							        <br/>
+							        <br/>
+							      </div>
+							    </div>
+							  </div>
+							  <div class="col-md-6">
+							    <div class="row">
+							      <div class="col-sm-4">
+							        <span class="footer-title">Community</span>
+							        <br>
+							        <a href="https://applyboard.zendesk.com/hc/en-us">Help Center</a>
+							        <br>
+							        <a href="https://applyboard.com/testimonials">How it works</a>
+							        <br>
+							        <a href="http://applyboard.com/blog">Blog</a>
+							        <br/>
+							        <br/>
+							      </div>
 
-        						<?php echo wp_kses_post( get_theme_mod( 'luna_footerid' )); // footer id ?>
+							      <div class="col-sm-4">
+							        <span class="footer-title">Quick Links</span>
+							        <br>
+							        <a href="https://applyboard.zendesk.com/hc/en-us/categories/200413168-Visa" target="_blank">Visa Guidance</a>
+							        <br>
+							        <a href="https://applyboard.zendesk.com/hc/en-us/categories/200415658-Study-in-Canada" target="_blank">Study in Canada</a>
+							        <br>
+							        <a href="https://applyboard.zendesk.com/hc/en-us/categories/201062757-Study-in-United-States-of-America" target="_blank">Study
+							          in USA</a>
+							        <br/>
+							        <a href="https://applyboard.zendesk.com/hc/en-us/articles/229458207" target="_blank">Fees</a>
+							        <br/>
+							      </div>
 
-							<?php else : ?>
+							      <div class="col-sm-4">
+							        <span class="footer-title">Contact Us</span>
+							        <br>
+							        Easy Education Inc.
+							        <br>
+							        30 Duke St. West, Suite 1100<br/>
+							        Kitchener, ON.,<br/>
+							        N2H 3W5 Canada<br/>
+							        <abbr title="Phone">P:</abbr> (519) 900-6001
+							        <br/>
+							        <br/>
+							      </div>
 
-<div class="row text-center-sm" style="margin-top:20px; margin-bottom:20px;">
-  <div class="col-md-6">
-    <div class="row">
-      <div class="col-sm-4">
-        <span class="footer-title">Welcome</span>
-        <br>
-				<a href="https://www.applyboard.com/quick_search">Find Programs</a>
-          <br>
-					<a href="https://www.applyboard.com/login">Login</a>
-          <br>
-					<a href="https://www.applyboard.com/register">Register</a>
-        	<br>
-					<a href="https://www.applyboard.com/forgot_password">Reset Password</a>
-	        <br/>
-        <br/>
-      </div>
+							    </div>
 
-      <div class="col-sm-4">
-        <span class="footer-title">About</span>
-        <br>
-				<a href="https://www.applyboard.com/about_us">Company</a>
-        <br/>
-        <a href="https://www.applyboard.com/features">How it works</a>
-        <br/>
-      </div>
+							  </div>
+							</div>
 
-      <div class="col-sm-4">
-        <span class="footer-title">Partners</span>
-        <br>
-				<a href="https://www.applyboard.com/schools">Schools</a>
-        <br>
-				<a href="https://www.applyboard.com/partner_with_us">Partner with Us</a>
-        <br>
-				<a href="https://www.applyboard.com/certificates">Certificates</a>
-        <br/>
-        <br/>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-6">
-    <div class="row">
-      <div class="col-sm-4">
-        <span class="footer-title">Community</span>
-        <br>
-        <a href="https://applyboard.zendesk.com/hc/en-us">Help Center</a>
-        <br>
-        <a href="https://applyboard.com/testimonials">How it works</a>
-        <br>
-        <a href="http://applyboard.com/blog">Blog</a>
-        <br/>
-        <br/>
-      </div>
-
-      <div class="col-sm-4">
-        <span class="footer-title">Quick Links</span>
-        <br>
-        <a href="https://applyboard.zendesk.com/hc/en-us/categories/200413168-Visa" target="_blank">Visa Guidance</a>
-        <br>
-        <a href="https://applyboard.zendesk.com/hc/en-us/categories/200415658-Study-in-Canada" target="_blank">Study in Canada</a>
-        <br>
-        <a href="https://applyboard.zendesk.com/hc/en-us/categories/201062757-Study-in-United-States-of-America" target="_blank">Study
-          in USA</a>
-        <br/>
-        <a href="https://applyboard.zendesk.com/hc/en-us/articles/229458207" target="_blank">Fees</a>
-        <br/>
-      </div>
-
-      <div class="col-sm-4">
-        <span class="footer-title">Contact Us</span>
-        <br>
-        Easy Education Inc.
-        <br>
-        30 Duke St. West, Suite 1100<br/>
-        Kitchener, ON.,<br/>
-        N2H 3W5 Canada<br/>
-        <abbr title="Phone">P:</abbr> (519) 900-6001
-        <br/>
-        <br/>
-      </div>
-
-    </div>
-
-  </div>
-</div>
-
-<div class="row">
-  <div class="col-lg-12 text-center">
-    <a href="mailto:info@applyboard.com" class="btn btn-primary">Send us email</a>
-    <p class="m-t-sm">
-      Or follow us on social platform
-    </p>
-    <ul class="list-inline social-icon">
-      <li><a target="_blank" href="https://twitter.com/applyboard"><i class="fa fa-twitter"></i></a>
-      </li>
-      <li><a target="_blank" href="https://www.facebook.com/applyboard"><i class="fa fa-facebook"></i></a>
-      </li>
-      <li><a target="_blank" href="https://www.youtube.com/c/ApplyboardChannel"><i class="fa fa-youtube"></i></a>
-      </li>
-      <li><a target="_blank" href="https://instagram.com/applyboard/"><i class="fa fa-instagram"></i></a>
-      </li>
-      <li><a target="_blank" href="https://plus.google.com/+ApplyboardChannel"><i class="fa fa-google-plus"></i></a>
-      </li>
-      <li><a target="_blank" href="https://www.linkedin.com/company/applyboard"><i class="fa fa-linkedin"></i></a>
-      </li>
-    </ul>
-  </div>
-</div>
-<div class="row">
-  <div class="col-lg-8 col-lg-offset-2 text-center m-t-lg m-b-lg">
-        <span>
-            <strong>&copy;
-              2017 ApplyBoard.com</strong> | <a href="https://applyboard.com/terms_of_service">Terms of Service</a> | <a href="https://applyboard.com/privacy_policy">Privacy Policy</a><br/>
-            An online tool for students from anywhere in the world to find the best university or college in Canada or the USA and apply to it.
-        </span>
-  </div>
-</div>
-
-							<?php endif; ?>
-
-                            <?php endif; ?>
-
-                        </div>
+							<div class="row">
+							  <div class="col-lg-12 text-center">
+							    <a href="mailto:info@applyboard.com" class="btn btn-primary">Send us email</a>
+							    <p class="m-t-sm">
+							      Or follow us on social platform
+							    </p>
+							    <ul class="list-inline social-icon">
+							      <li><a target="_blank" href="https://twitter.com/applyboard"><i class="fa fa-twitter"></i></a>
+							      </li>
+							      <li><a target="_blank" href="https://www.facebook.com/applyboard"><i class="fa fa-facebook"></i></a>
+							      </li>
+							      <li><a target="_blank" href="https://www.youtube.com/c/ApplyboardChannel"><i class="fa fa-youtube"></i></a>
+							      </li>
+							      <li><a target="_blank" href="https://instagram.com/applyboard/"><i class="fa fa-instagram"></i></a>
+							      </li>
+							      <li><a target="_blank" href="https://plus.google.com/+ApplyboardChannel"><i class="fa fa-google-plus"></i></a>
+							      </li>
+							      <li><a target="_blank" href="https://www.linkedin.com/company/applyboard"><i class="fa fa-linkedin"></i></a>
+							      </li>
+							    </ul>
+							  </div>
+							</div>
+							<div class="row">
+							  <div class="col-lg-8 col-lg-offset-2 text-center m-t-lg m-b-lg">
+							        <span>
+							            <strong>&copy;
+							              2017 ApplyBoard.com</strong> | <a href="https://applyboard.com/terms_of_service">Terms of Service</a> | <a href="https://applyboard.com/privacy_policy">Privacy Policy</a><br/>
+							            An online tool for students from anywhere in the world to find the best university or college in Canada or the USA and apply to it.
+							        </span>
+							  </div>
+							</div>
 
 
-                    	<?php if( get_theme_mod( 'active_social' ) == '') : ?>
 
-                    		<?php if ( is_active_sidebar('social-widget-area') ) : ?>
-
-                        		<div class="col-1-2">
-                            		<?php dynamic_sidebar('social-widget-area');  ?>
-                            	</div>
-
-                    		<?php endif; ?>
-
-                    	<?php endif; ?>
-
-
-                </div>
-            </div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
